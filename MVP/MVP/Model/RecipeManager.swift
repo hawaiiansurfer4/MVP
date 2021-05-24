e23da23a411cad6f295c841203efeca9b0449435
Sean Murphy <sdmurphy.1999@gmail.com>
2021-05-23 23:39:05 -0700

    transition fully working

--- MVP/MVP/Model/RecipeManager.swift
+++ MVP/MVP/Model/RecipeManager.swift
@@ -12,21 +12,21 @@ protocol RecipeManagerDelegate {
     func didFailWithError(error: Error)
 }
 
-class ReceipeManagerMultiCastDelegate: NSObject, RecipeManagerDelegate{
-    
+class ReceipeManagerMultiCastDelegate: NSObject, RecipeManagerDelegate {
+
     let multicast = MulticastDelegate<RecipeManagerDelegate>()
-    
+
     init(delegates: [RecipeManagerDelegate]) {
         super.init()
         delegates.forEach(multicast.add)
     }
-    
+
     func didUpdateRecipe(_ recipeManager: RecipeManager, recipeModel: RecipeModel) {
-        multicast.invoke{$0.didUpdateRecipe(recipeManager, recipeModel: recipeModel)}
+        multicast.invoke { $0.didUpdateRecipe(recipeManager, recipeModel: recipeModel) }
     }
-    
+
     func didFailWithError(error: Error) {
-        multicast.invoke{$0.didFailWithError(error: error)}
+        multicast.invoke { $0.didFailWithError(error: error) }
     }
 }
 
@@ -35,43 +35,43 @@ struct RecipeManager {
     let appID = "3214dd26"
     let appKey = "24f428ea7ca46ce12a04eabda6c59909"
     let maxNumberOfApiRequests = 100
-    
+
     static let shared = RecipeManager()
     var delegateManager: ReceipeManagerMultiCastDelegate
     private init() {
         self.delegateManager = ReceipeManagerMultiCastDelegate(delegates: [])
     }
-    
+
     static var recipeArray = [String]()
     var delegate: RecipeManagerDelegate?
-    
+
     func fetchRecipe(typeOfFood: String) {
-        
+
         let urlString = "\(recipeURL)&app_id=\(appID)&app_key=\(appKey)&q=\(typeOfFood)&from=0&to=\(maxNumberOfApiRequests)"
 //        print(urlString)
         performRequest(urlString: urlString)
     }
 
-        
+
     func performRequest(urlString: String) {
-        
+
 //        let recipeTableVC = RecipeTableViewController()
 //        recipeTableVC.status = .loading
-        
+
         if let url = URL(string: urlString) {
-            
+
             let session = URLSession(configuration: .default)
-            
+
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
 //                    self.delegate?.didFailWithError(error: error!)
                     delegateManager.didFailWithError(error: error!)
                     return
                 }
-                
+
                 if let safeData = data {
                     if let recipeModel = parseJSON(safeData) {
-                        
+
 //                        self.delegate?.didUpdateRecipe(self, recipeModel: recipeModel)
                         delegateManager.didUpdateRecipe(self, recipeModel: recipeModel)
                     }
@@ -80,7 +80,7 @@ struct RecipeManager {
             task.resume()
         }
     }
-    
+
     func parseJSON(_ recipeData: Data) -> RecipeModel? {
         let decoder = JSONDecoder()
         var recipeList = [String]()
@@ -88,16 +88,16 @@ struct RecipeManager {
         do {
 //            recipeList.removeAll()
 //            urlList.removeAll()
-            
+
 //            print("This is my recipe list \(recipeList)")
 //            print("This is my url list \(urlList)")
             let decodedData = try decoder.decode(RecipeData.self, from: recipeData)
-            
+
             for i in 0..<maxNumberOfApiRequests {
                 recipeList.append(contentsOf: [decodedData.hits[i].recipe.label])
                 urlList.append(contentsOf: [decodedData.hits[i].recipe.url])
             }
-            
+
 //            for j in 0..<maxNumberOfApiRequests {
 //                urlList.append(contentsOf: [decodedData.hits[j].recipe.url])
 //            }
@@ -110,7 +110,7 @@ struct RecipeManager {
             self.delegate?.didFailWithError(error: error)
             return nil
         }
-        
+
     }
 }
 
