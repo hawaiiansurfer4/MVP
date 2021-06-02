5f9e0b24d102984e053ca107d74747717995c13c
Sean Murphy <sdmurphy.1999@gmail.com>
2021-06-01 17:02:06 -0700

    Saved using core data

--- MVP/MVP/Model/SearchHistoryModel.swift
+++ MVP/MVP/Model/SearchHistoryModel.swift
@@ -6,10 +6,12 @@
 //
 
 import UIKit
+import CoreData
 
 
 class SearchHistoryModel {
     private var historyStorage: [String] = []
+    private var defaults = UserDefaults.standard
     private var count = 0 {
         didSet {
             if count > CAPACITY {
@@ -21,16 +23,20 @@ class SearchHistoryModel {
 
     func push(_ recentlySearched: String) {
         historyStorage.append(recentlySearched)
+        defaults.set(historyStorage, forKey: "SearchHistoryArray")
         count+=1
     }
 
     func pop() -> Void {
         historyStorage.removeFirst()
+        defaults.set(historyStorage, forKey: "SearchHistoryArray")
         count-=1
     }
 
     func searchPopulation() -> [String] {
-
+        if let items = defaults.array(forKey: "SearchHistoryArray") as? [String] {
+            historyStorage = items
+        }
         return historyStorage.map{$0}.reversed()    }
 
 
