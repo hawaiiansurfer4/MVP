5564ed3cafee3dda986a6d957a6e93e39c0130f7
Sean Murphy <sdmurphy.1999@gmail.com>
2021-07-16 09:48:14 -0700

    Adding native Vision framework and function to recognize text from a picture

--- MVP/MVP/Controller/ScannerViewController.swift
+++ MVP/MVP/Controller/ScannerViewController.swift
@@ -6,7 +6,46 @@
 //
 
 import UIKit
+import Vision
 
-class ScannerViewController: UIViewController {
+class ScannerViewController: UIViewController  {
     
+    
+    
+    
+    @IBOutlet weak var cameraScanner: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func scannnerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    func recognizeText(image: UIImage?) {
+        guard let cgImage = image?.cgImage else { return }
+        
+        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        
+        let request = VNDetectTextRectanglesRequest { request, error in
+            guard let observations = request.results as? [VNRecognizedTextObservation],
+                  error == nil else {
+                return
+            }
+            
+            let text = observations.compactMap({
+                $0.topCandidates(1).first?.string
+            }).joined()
+        }
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
 }
+    
+
