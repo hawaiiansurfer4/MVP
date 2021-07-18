4eb0058a77f469e1d18dc6679d9170d8f0c3b342
Sean Murphy <sdmurphy.1999@gmail.com>
2021-07-18 13:53:32 -0700

    Adding access to camera and photo library

--- MVP/MVP/Controller/ScannerViewController.swift
+++ MVP/MVP/Controller/ScannerViewController.swift
@@ -23,24 +23,46 @@ class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, 
         imagePicker.sourceType = .camera
         tellUser.text = "Press the scan button to take a picture"
         tellUser.numberOfLines = 0
-//        tellUser.center = CGPoint(x: CGPoin, y: 0)
-//        imagePicker.
         
     }
     
-    @IBAction func scannnerButtonPressed(_ sender: UIButton) {
-//        if imagePicker != nil {
-            present(imagePicker, animated: true, completion: nil)
-//        } else {
-//            tellUser.text = "Please take a picture or select a picture to continue"
-//        }
+    @IBAction func cameraScannerButtonPressed(_ sender: UIButton) {
+        getImage(fromSourceType: .camera)
+        present(imagePicker, animated: true, completion: nil)
+        tellUser.text = ""
+    }
+    
+    @IBAction func photoLibraryButtonPressed(_ sender: UIButton) {
+        getImage(fromSourceType: .photoLibrary)
+        present(imagePicker, animated: true, completion: nil)
+        tellUser.text = ""
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
-        cameraScannerView.image = userPickedImage as? UIImage
+        self.dismiss(animated: true) {
+            [weak self] in
         
-        imagePicker.dismiss(animated: true, completion: nil)
+        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
+            self!.cameraScannerView.image = userPickedImage as? UIImage
+        
+            self!.imagePicker.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    private func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
+
+        //Check is source type available
+        if UIImagePickerController.isSourceTypeAvailable(sourceType) {
+
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.delegate = self
+            imagePickerController.sourceType = sourceType
+            self.present(imagePickerController, animated: true, completion: nil)
+        }
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
     
     func recognizeText(image: UIImage?) {
@@ -66,5 +88,38 @@ class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
     }
 }
+
+//MARK: - Scanner camera access code
+
+//Show alert to selected the media source type.
+//   private func showAlert() {
+//
+//       let alert = UIAlertController(title: "Image Selection", message: "From where you want to pick this image?", preferredStyle: .actionSheet)
+//       alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
+//           self.getImage(fromSourceType: .camera)
+//       }))
+//       alert.addAction(UIAlertAction(title: "Photo Album", style: .default, handler: {(action: UIAlertAction) in
+//           self.getImage(fromSourceType: .photoLibrary)
+//       }))
+//       alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+//       self.present(alert, animated: true, completion: nil)
+//   }
+
+
+   //MARK:- UIImagePickerViewDelegate.
+//   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//
+//       self.dismiss(animated: true) { [weak self] in
+//
+//           guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+//           //Setting image to your image view
+//           self?.profileImgView.image = image
+//       }
+//   }
+//
+//   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//       picker.dismiss(animated: true, completion: nil)
+//   }
+
     
 
