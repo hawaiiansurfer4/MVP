aaae620b82ade3a02078be6cdf3930f5d2def9a8
Sean Murphy <sdmurphy.1999@gmail.com>
2021-07-18 19:25:31 -0700

    Pictures Converting to text

--- MVP/MVP/Controller/ScannerViewController.swift
+++ MVP/MVP/Controller/ScannerViewController.swift
@@ -20,21 +20,21 @@ class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, 
         super.viewDidLoad()
         imagePicker.delegate = self
         imagePicker.allowsEditing = false
-        imagePicker.sourceType = .camera
         tellUser.text = "Press the scan button to take a picture"
         tellUser.numberOfLines = 0
+        cameraScannerView.contentMode = .scaleAspectFit
         
     }
     
     @IBAction func cameraScannerButtonPressed(_ sender: UIButton) {
         getImage(fromSourceType: .camera)
-        present(imagePicker, animated: true, completion: nil)
+//        present(imagePicker, animated: true, completion: nil)
         tellUser.text = ""
     }
     
     @IBAction func photoLibraryButtonPressed(_ sender: UIButton) {
         getImage(fromSourceType: .photoLibrary)
-        present(imagePicker, animated: true, completion: nil)
+//        present(imagePicker, animated: true, completion: nil)
         tellUser.text = ""
     }
     
@@ -42,35 +42,49 @@ class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, 
         self.dismiss(animated: true) {
             [weak self] in
         
-        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
-            self!.cameraScannerView.image = userPickedImage as? UIImage
-        
-            self!.imagePicker.dismiss(animated: true, completion: nil)
+            if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] {
+                self!.cameraScannerView.image = userPickedImage as? UIImage
+            
+                self!.imagePicker.dismiss(animated: true, completion: nil)
+                self?.recognizeText(image: userPickedImage as! UIImage)
+            }
+            
+//            getImage(fromSourceType: userPickedImage )
         }
     }
     
     private func getImage(fromSourceType sourceType: UIImagePickerController.SourceType) {
 
-        //Check is source type available
         if UIImagePickerController.isSourceTypeAvailable(sourceType) {
 
-            let imagePickerController = UIImagePickerController()
-            imagePickerController.delegate = self
-            imagePickerController.sourceType = sourceType
-            self.present(imagePickerController, animated: true, completion: nil)
+            imagePicker.sourceType = sourceType
+            self.present(imagePicker, animated: true, completion: nil)
+            tellUser.text = ""
+            if imagePicker.isViewLoaded {
+                
+            }
         }
     }
-
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
+    
+    func showAlert() {
+        let alert = UIAlertController(title: "Image Selected", message: "Are you sure that you want to continue with the image that you selected?", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { (action: UIAlertAction) in
+            self.recognizeText(image: self.imagePicker as? UIImage)
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
+
+//    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+//        picker.dismiss(animated: true, completion: nil)
+//    }
     
     func recognizeText(image: UIImage?) {
         guard let cgImage = image?.cgImage else { return }
         
         let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
         
-        let request = VNDetectTextRectanglesRequest { request, error in
+        let request = VNRecognizeTextRequest { request, error in
             guard let observations = request.results as? [VNRecognizedTextObservation],
                   error == nil else {
                 return
@@ -79,47 +93,17 @@ class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, 
             let text = observations.compactMap({
                 $0.topCandidates(1).first?.string
             }).joined()
+            print("here is the text that should be printed \(text)")
         }
         
         do {
-            try handler.perform([request])
+            let statementToPrint = try handler.perform([request])
+            print("Here is the translated text \(statementToPrint)")
         } catch {
             print(error)
         }
     }
 }
-
-//MARK: - Scanner camera access code
-
-//Show alert to selected the media source type.
-//   private func showAlert() {
-//
-//       let alert = UIAlertController(title: "Image Selection", message: "From where you want to pick this image?", preferredStyle: .actionSheet)
-//       alert.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(action: UIAlertAction) in
-//           self.getImage(fromSourceType: .camera)
-//       }))
-//       alert.addAction(UIAlertAction(title: "Photo Album", style: .default, handler: {(action: UIAlertAction) in
-//           self.getImage(fromSourceType: .photoLibrary)
-//       }))
-//       alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-//       self.present(alert, animated: true, completion: nil)
-//   }
-
-
-   //MARK:- UIImagePickerViewDelegate.
-//   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-//
-//       self.dismiss(animated: true) { [weak self] in
-//
-//           guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
-//           //Setting image to your image view
-//           self?.profileImgView.image = image
-//       }
-//   }
-//
-//   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-//       picker.dismiss(animated: true, completion: nil)
-//   }
 
     
 
