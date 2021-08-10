d1cec98b5bf6633a9386d2a24819094e935f8d6a
Sean Murphy <sdmurphy.1999@gmail.com>
2021-08-09 18:42:06 -0700

    savedReceipes working properly adding UITableView over a UIView

--- MVP/MVP/Controller/ScannerViewController.swift
+++ MVP/MVP/Controller/ScannerViewController.swift
@@ -9,15 +9,15 @@ import UIKit
 import Vision
 import CoreData
 
-class ScannerViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+class ScannerViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITableViewDelegate, UITableViewDataSource  {
     
     
+    @IBOutlet weak var savedReceipesTableOverlay: UITableView!
     let imagePicker = UIImagePickerController()
     var savedReceipeArray = [SavedReceipes]()
-//    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-//    let context = NSEntityDescription.insertNewObject(forEntityName: "SavedReceipes", into: NSManagedObjectContext) as! RecipeContext
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     var scannedText = ""
+    let stupid = ["you", "should", "focus", "stupid"]
     
     var count = 0 {
         didSet {
@@ -35,9 +35,11 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
         super.viewDidLoad()
         imagePicker.delegate = self
         imagePicker.allowsEditing = false
-//        tellUser.text = "Press the scan button to take a picture"
-//        tellUser.numberOfLines = 0
-        
+        savedReceipesTableOverlay.delegate = self
+        savedReceipesTableOverlay.dataSource = self
+        savedReceipesTableOverlay.becomeFirstResponder()
+        savedReceipesTableOverlay.reloadInputViews()
+        loadReceipes()
     }
     
     @IBAction func cameraScannerButtonPressed(_ sender: UIBarButtonItem) {
@@ -58,8 +60,6 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
                 self!.imagePicker.dismiss(animated: true, completion: nil)
                 self?.recognizeText(image: userPickedImage as! UIImage)
             }
-            
-//            getImage(fromSourceType: userPickedImage )
         }
     }
     
@@ -74,24 +74,12 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
             }
         }
     }
-    
-//    func showAlert() {
-//        let alert = UIAlertController(title: "Image Selected", message: "Are you sure that you want to continue with the image that you selected?", preferredStyle: .actionSheet)
-//        alert.addAction(UIAlertAction(title: "Continue", style: .default, handler: { (action: UIAlertAction) in
-//            self.recognizeText(image: self.imagePicker as? UIImage)
-//        }))
-//        alert.addAction(UIAlertAction(title: "Cancel", style: .destructive, handler: nil))
-//        self.present(alert, animated: true, completion: nil)
-//    }
 
 //    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
 //        picker.dismiss(animated: true, completion: nil)
 //    }
     
     func recognizeText(image: UIImage?) {
-//        guard let cgImage = image?.cgImage else { return }
-        
-//        let handler = VNImageRequestHandler(cgImage: cgImage, options: [:])
         
         
         let request = VNRecognizeTextRequest { [weak self] request, error in
@@ -105,84 +93,46 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
                     print("No candidate")
                     continue
                 }
-                
-//                print("Found this candidate: \(text.string)")
                 compactedString.append(text.string)
-//                self?.updateReceipe()
             }
             self?.scannedText = compactedString.joined(separator: " ")
-            print("Here is the completed scan \(self?.scannedText)")
             
-//            let text = observations.compactMap({
-//                $0.topCandidates(1).first?.string
-//            }).joined(separator: " ")
-//            print("###################   here is the text that should be printed \(text)")
-//            DispatchQueue.main.async {
-//                self?.updateReceipe(text)
-//            }
-//            self.updateReceipe(text)
         }
-//        guard let completedScan = scannedText else { return }
         request.recognitionLevel = .accurate
         let requests = [request]
-        DispatchQueue.global(qos: .userInitiated).async {
             guard let cgImage = image?.cgImage else {
                 fatalError("Missing image to scan")
             }
             let handler = VNImageRequestHandler(cgImage: cgImage, options: [ : ])
-            let testing = try? handler.perform(requests)
-//            print("HERE IS THE TESTING $$$$%%%#%#%#%$%$%$%$%#%#%$%$%$% \(testing)")
-            self.updateReceipe(self.scannedText)
+            if let testing = try? handler.perform(requests) {
+                self.updateReceipe(self.scannedText)
         }
 
-//        let convert = request.results
-//        print("Here is the conversion @@@@@@@@@ \(convert)")
-//        do {
-//            let statementToPrint = try handler.perform([request])
-//            print("$$$$$$$$$$$$$$$$$$    Here is the translated text \(statementToPrint)")
-//        } catch {
-//            print(error)
-//        }
     }
     
     func updateReceipe(_ latestScan: String) {
-        DispatchQueue.main.async {
-            let newReceipe = SavedReceipes(context: self.context)
-            newReceipe.receipe = latestScan
-//            do {
-//                try self.context.save()
-//            } catch {
-//                print("You done fucked up")
-//            }
-            self.saveReceipes()
-            self.loadReceipes()
-        }
-//        let newReceipe = SavedReceipes(context: context)
-//        newReceipe.receipe = latestScan
-//        do {
-//            try context.save()
-//        } catch {
-//            print("You done fucked up")
-//        }
-////        self.saveReceipes()
-//        self.loadReceipes()
+        let newReceipe = SavedReceipes(context: context)
+        newReceipe.receipe = latestScan
+        self.saveReceipes()
     }
     
     //MARK: - TableView Methods
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "savedReceipeCell")
-        cell?.textLabel?.text = savedReceipeArray[indexPath.row].receipe
-        cell?.textLabel?.numberOfLines = 0
-        cell?.accessoryType = .none
-        return cell!
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Table view called \(indexPath)")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "savedReceipeCell", for: indexPath)
+        cell.textLabel?.text = savedReceipeArray[indexPath.row].receipe ?? "NOthing to show for the table"
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.adjustsFontForContentSizeCategory = true
+        cell.accessoryType = .none
+        return cell
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return savedReceipeArray.count
     }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
@@ -195,7 +145,8 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
         } catch {
            print("Error saving context \(error)")
         }
-        tableView.reloadData()
+//        loadReceipes()
+        savedReceipesTableOverlay.reloadData()
     }
     
     func loadReceipes(with request: NSFetchRequest<SavedReceipes> = SavedReceipes.fetchRequest(), predicate: NSPredicate? = nil) {
@@ -207,7 +158,7 @@ class ScannerViewController: UITableViewController, UIImagePickerControllerDeleg
             print("Error fetching data from context \(error)")
         }
         
-        tableView.reloadData()
+        savedReceipesTableOverlay.reloadData()
         
     }
 }
