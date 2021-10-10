4ff6a1ddd555768088e0584d25fcb8dfe6a47c02
Sean Murphy <sdmurphy.1999@gmail.com>
2021-10-10 12:09:50 -0400

    FilterVC with working scrollview

---
+++ MVP/MVP/Controller/FilterVC.swift
@@ -0,0 +1,18 @@
+//
+//  filterVC.swift
+//  MVP
+//
+//  Created by Sean Murphy on 10/9/21.
+//
+
+import UIKit
+
+class FilterVC: UIViewController, UIScrollViewDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+}
