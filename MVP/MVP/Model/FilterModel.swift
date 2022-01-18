b3d76eb90ffd24895bc2bacbeef3a3936608300a
Sean Murphy <sdmurphy.1999@gmail.com>
2022-01-18 12:01:36 -0500

    adds filter cell

--- MVP/MVP/Model/FilterModel.swift
+++ MVP/MVP/Model/FilterModel.swift
@@ -11,9 +11,9 @@ import UIKit
 var appFirstLoad = true
 
 class FilterModel {
-    
-    
-    var allergyList = ["Alcohol-free", "Immune-Supportive", "Celery-free", "Crustcean-free", "Dairy", "Dietary Approaches to Stop Hypertension","Eggs", "Fish", "FODMAP free", "Gluten", "Keto", "Kidney friendly", "Kosher", "Low potassium", "Lupine-free", "Mediterranean", "Mustard-free", "Low-fat-abs", "No oil added", "No-sugar", "Paleo", "Peanuts", "Pescatarian", "Pork-free", "Red meat-free", "Sesame-free", "Shellfish", "Soy", "Sugar-conscious", "Tree Nuts", "Vegan", "Vegetarian", "Wheat-free"]
+
+
+    var allergyList = ["Alcohol-free", "Immune-Supportive", "Celery-free", "Crustcean-free", "Dairy", "Dietary Approaches to Stop Hypertension", "Eggs", "Fish", "FODMAP free", "Gluten", "Keto", "Kidney friendly", "Kosher", "Low potassium", "Lupine-free", "Mediterranean", "Mustard-free", "Low-fat-abs", "No oil added", "No-sugar", "Paleo", "Peanuts", "Pescatarian", "Pork-free", "Red meat-free", "Sesame-free", "Shellfish", "Soy", "Sugar-conscious", "Tree Nuts", "Vegan", "Vegetarian", "Wheat-free"]
 
     var dietList = ["Balanced", "High-Fiber", "High-Protein", "Low-Carb", "Low-Fat", "Low-Sodium"]
 
@@ -22,42 +22,45 @@ class FilterModel {
     var foodList = ["Alcohol-cocktail", "Biscuits and cookies", "Bread", "Cereals", "Condiments and sauces", "Drinks", "Desserts", "Egg", "Main course", "Omelet", "Pancake", "Preps", "Preserve", "Salad", "Sandwhiches", "Soup", "Starter"]
 
     var cuisineTypeList = ["Asmerican", "Asian", "British", "Caribbean", "Central Europe", "Chinese", "Eastern Europe", "French", "Indian", "Italian", "Japanese", "Kosher", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "South American", "South East Asian"]
-    
+
     func addFilters() {
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
-        let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
-        let newFilter = Filters(entity: entity!, insertInto: context)
-        newFilter.category = "Allergies"
+        if appFirstLoad {
+            appFirstLoad = false
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.category = "Allergies"
 //        filterList.append("Allergies")
-        for allergy in allergyList {
-            newFilter.filter = allergy
-        }
-        newFilter.category = "Diet"
-        for diet in dietList {
-            newFilter.filter = diet
-        }
-        newFilter.category = "Meal"
-        for meal in mealList {
-            newFilter.filter = meal
-        }
-        newFilter.category = "Food"
-        for food in foodList {
-            newFilter.filter = food
-        }
-        newFilter.category = "Cuisine"
-        for cuisine in cuisineTypeList {
-            newFilter.filter = cuisine
-        }
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Filters")
-        do {
-            let results: NSArray = try context.fetch(request) as NSArray
-            for result in results {
-                let filters = result as! Filters
-                filterList.append(filters)
+            for allergy in allergyList {
+                newFilter.filter = allergy
             }
-        } catch {
-            print("Fetch Failed for filter")
+            newFilter.category = "Diet"
+            for diet in dietList {
+                newFilter.filter = diet
+            }
+            newFilter.category = "Meal"
+            for meal in mealList {
+                newFilter.filter = meal
+            }
+            newFilter.category = "Food"
+            for food in foodList {
+                newFilter.filter = food
+            }
+            newFilter.category = "Cuisine"
+            for cuisine in cuisineTypeList {
+                newFilter.filter = cuisine
+            }
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Filters")
+            do {
+                let results: NSArray = try context.fetch(request) as NSArray
+                for result in results {
+                    let filters = result as! Filters
+                    filterList.append(filters)
+                }
+            } catch {
+                print("Fetch Failed for filter")
+            }
         }
     }
 }
---
+++ MVP/MVP/View/FilterTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FilterTableViewCell.swift
+//  MVP
+//
+//  Created by Sean Murphy on 1/18/22.
+//
+
+import UIKit
+
+class FilterTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
