14e565b22e5f5fea52e7b2f382b7be501b4ea2e0
Sean Murphy <sdmurphy.1999@gmail.com>
2022-03-11 14:57:07 -0500

    adds all filters and checks if all filters have already been added or not

--- MVP/MVP/Controller/FilterVC.swift
+++ MVP/MVP/Controller/FilterVC.swift
@@ -8,34 +8,28 @@
 import UIKit
 import CoreData
 
-//var filterList = [Filters]()
 var loaded = false
 
 class FilterVC: UITableViewController {
-    
+
     var filterList = [Filters]()
-//    let filterContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    var keepCount = 0
-    var screenLoad = true
+    var filter = Filters.self
+    var filtersInPlace = false
     var previousCategory = "None"
     var allergyList = ["Alcohol-free", "Immune-Supportive", "Celery-free", "Crustcean-free", "Dairy", "Dietary Approaches to Stop Hypertension", "Eggs", "Fish", "FODMAP free", "Gluten", "Keto", "Kidney friendly", "Kosher", "Low potassium", "Lupine-free", "Mediterranean", "Mustard-free", "Low-fat-abs", "No oil added", "No-sugar", "Paleo", "Peanuts", "Pescatarian", "Pork-free", "Red meat-free", "Sesame-free", "Shellfish", "Soy", "Sugar-conscious", "Tree Nuts", "Vegan", "Vegetarian", "Wheat-free"]
-    
-//    var allergyList = ["Alcohol-free", "Immune-Supportive", "Celery-free"]
-//    var allergyID = ["alcohol-free", "immuno-supportive", "celery-free"]
+
 
     var allergyID = ["alcohol-free", "immuno-supportive", "celery-free", "crustacean-free", "dairy-free", "DASH", "egg-free", "fish-free", "fodmap-free", "gluten-free", "keto-friendly", "gluten-free", "keto-friendly", "kidney-friendly", "kosher", "low-potassium", "lupine-free", "Mediterranean", "mustard-free", "low-fat-abs", "No-oil-added", "low-sugar", "paleo", "peanut-free", "pecatarian", "pork-free", "red-meat-free", "seasame-free", "shellfish-free", "soy-free", "sugar-conscious", "tree-nut-free", "vegan", "vegetarian", "wheat-free"]
-//
-//    var dietList = ["Balanced", "High-Fiber", "High-Protein", "Low-Carb", "Low-Fat", "Low-Sodium"]
-//
-//    var dietID = ["balanced", "high-fiber", "high-protein", "low-carb", "low-fat", "low-sodium"]
-//
-//    var mealList = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"]
-//
-//    var typeOfDishList = ["Alcohol-cocktail", "Biscuits and cookies", "Bread", "Cereals", "Condiments and sauces", "Drinks", "Desserts", "Egg", "Main course", "Omelet", "Pancake", "Preps", "Preserve", "Salad", "Sandwhiches", "Soup", "Starter"]
-//
-//    var cuisineTypeList = ["Asmerican", "Asian", "British", "Caribbean", "Central Europe", "Chinese", "Eastern Europe", "French", "Indian", "Italian", "Japanese", "Kosher", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "South American", "South East Asian"]
 
-    var countingTotal = 0
+    var dietList = ["Balanced", "High-Fiber", "High-Protein", "Low-Carb", "Low-Fat", "Low-Sodium"]
+
+    var dietID = ["balanced", "high-fiber", "high-protein", "low-carb", "low-fat", "low-sodium"]
+
+    var mealList = ["Breakfast", "Lunch", "Dinner", "Snack", "Teatime"]
+
+    var typeOfDishList = ["Alcohol-cocktail", "Biscuits and cookies", "Bread", "Cereals", "Condiments and sauces", "Drinks", "Desserts", "Egg", "Main course", "Omelet", "Pancake", "Preps", "Preserve", "Salad", "Sandwhiches", "Soup", "Starter"]
+
+    var cuisineTypeList = ["Asmerican", "Asian", "British", "Caribbean", "Central Europe", "Chinese", "Eastern Europe", "French", "Indian", "Italian", "Japanese", "Kosher", "Mediterranean", "Mexican", "Middle Eastern", "Nordic", "South American", "South East Asian"]
 
 
     override func viewDidLoad() {
@@ -53,9 +47,6 @@ class FilterVC: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "FilterCell", for: indexPath) as! FilterTableViewCell
-        if filterList[indexPath.row].isSelected == nil {
-            filterList[indexPath.row].isSelected = false
-        }
         if filterList[indexPath.row].categoryTitle != previousCategory {
             cell.filterLabel.text = filterList[indexPath.row].categoryTitle
             cell.backgroundColor = .red
@@ -63,7 +54,8 @@ class FilterVC: UITableViewController {
         cell.filterLabel.text = filterList[indexPath.row].filter
 
         cell.isSelectedImage.image = filterList[indexPath.row].isSelected ? .checkmark : .none
-        
+        saveFilters()
+
         return cell
     }
 
@@ -84,28 +76,55 @@ class FilterVC: UITableViewController {
         tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
-    
+
     func hasFiltersBeenAdded() -> Bool {
-        // look at the core data for this entity and retrieve info and get the number of allergies and it is equal to the number of allergies in the table if it returns true skip addFilters
-       
-        return
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Filters")
+        let totalCount = allergyList.count + mealList.count + cuisineTypeList.count + dietList.count + typeOfDishList.count
+        do {
+            let results: NSArray = try context.fetch(request) as NSArray
+            if results.count == totalCount {
+                filtersInPlace = true
+            } else {
+                filtersInPlace = false
+            }
+        } catch {
+            print("Error checking if filters are in place")
+        }
+        return filtersInPlace
     }
 
     func addFilters() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
 
-            for allergy in 0..<self.allergyList.count {
-                let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
-                let newFilter = Filters(entity: entity!, insertInto: context)
-                self.countingTotal += 1
-                newFilter.categoryTitle = "Allergies"
-                newFilter.id = allergyID[allergy]
-                newFilter.filter = allergyList[allergy]
-                newFilter.isSelected = false
-                print("The value of newFilter is \(newFilter.filter)")
+        for allergy in 0..<self.allergyList.count {
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.categoryTitle = "Allergies"
+            newFilter.id = allergyID[allergy]
+            newFilter.filter = allergyList[allergy]
+            newFilter.isSelected = false
+            print("The value of newFilter is \(newFilter.filter)")
 
-            }
+        }
+        do {
+            try context.save()
+        } catch {
+            print("*** error saving filters to the context within saveFilters func ***")
+        }
+
+        for diet in 0..<self.dietList.count {
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.categoryTitle = "Diet"
+            newFilter.id = dietList[diet]
+            newFilter.filter = dietID[diet]
+            newFilter.isSelected = false
+            print("The value of newFilter is \(newFilter.filter)")
+
+        }
         do {
             try context.save()
         } catch {
@@ -113,15 +132,61 @@ class FilterVC: UITableViewController {
         }
         tableView.reloadData()
 
+        for meal in mealList {
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.categoryTitle = "Meal"
+            newFilter.id = meal
+            newFilter.filter = meal
+            newFilter.isSelected = false
+            print("The value of newFilter is \(newFilter.filter)")
+
+        }
+        do {
+            try context.save()
+        } catch {
+            print("*** error saving filters to the context within saveFilters func ***")
+        }
+        
+        for dish in typeOfDishList {
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.categoryTitle = "Dish Type"
+            newFilter.id = dish
+            newFilter.filter = dish
+            newFilter.isSelected = false
+            print("The value of newFilter is \(newFilter.filter)")
+
+        }
+        do {
+            try context.save()
+        } catch {
+            print("*** error saving filters to the context within saveFilters func ***")
+        }
+        
+        for cuisine in cuisineTypeList {
+            let entity = NSEntityDescription.entity(forEntityName: "Filters", in: context)
+            let newFilter = Filters(entity: entity!, insertInto: context)
+            newFilter.categoryTitle = "Cuisine"
+            newFilter.id = cuisine
+            newFilter.filter = cuisine
+            newFilter.isSelected = false
+            print("The value of newFilter is \(newFilter.filter)")
+
+        }
+        do {
+            try context.save()
+        } catch {
+            print("*** error saving filters to the context within saveFilters func ***")
+        }
+
     }
 
     func saveFilters() {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context = appDelegate.persistentContainer.viewContext
-        keepCount += 1
         do {
             try context.save()
-            print("I have been called \(keepCount) ")
         } catch {
             print("error saving filters to the context within saveFilters func")
         }
@@ -131,6 +196,7 @@ class FilterVC: UITableViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let context: NSManagedObjectContext = appDelegate.persistentContainer.viewContext
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Filters")
+        
         do {
             let results: NSArray = try context.fetch(request) as NSArray
             filterList.removeAll()
